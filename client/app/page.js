b1bc3d8d32d323cf53416a00a935258ff261906c
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-04-06 22:02:06 +0200

    Updated connectWallet Button

--- client/app/page.js
+++ client/app/page.js
@@ -1,15 +1,37 @@
-import NavBar from "@/components/NavBar";
+"use client"
+
+import ConnectWalletButton from "@/components/NavBar";
 import Description from "@/components/Description";
 import PageBottom from "@/components/PageBottom";
 import ToDoList from "@/components/ToDoList";
+import { ethers } from "ethers";
 
 export default function Home() {
-  
+
+  const connectWallet = async () => {
+    try {
+        const {ethereum} = window
+        if(!ethereum){
+          console.log('Metamask not detected')
+          return
+        }
+        let chainId = await ethereum.request({method: 'eth_chainId'})
+        console.log('connected to chain:',chainId)
+
+        const ethereumId = '0xaa36a7'
+        if(chainId !== ethereumId) {
+          alert('You are not connected to Ethereum Mainet')
+        }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
 
       {/* If not connected display */}
-      <NavBar/>
+      <ConnectWalletButton connectWallet={connectWallet}/>
       <Description/>
       {/* If connected display */}
         <ToDoList/>
--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const NavBar = () => {
+const ConnectWalletButton = ({connectWallet}) => {
   return (
     <>
       <div className='w-[full] flex px-8 py-8 justify-between'>
       <div className='flex-1 flex md:place-content-end place-content-center '>
         <button
           className="h-[4rem] text-2xl bg-blue-500  hover:bg-blue-700 text-white font-bold px-10 rounded-lg transition duration-400 ease-linear"
+          onClick={connectWallet}
           >
           Connect Wallet
         </button>
@@ -16,4 +17,4 @@ const NavBar = () => {
 )
 }
 
-export default NavBar
+export default ConnectWalletButton
