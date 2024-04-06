bce16d3fa88a3d973b3fb40c8443f5bad4f4e5b0
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-04-06 19:17:43 +0200

    added favicon

--- client/app/layout.js
+++ client/app/layout.js
@@ -3,6 +3,11 @@ import "./globals.css";
 export const metadata = {
   title: "ToDoChain ",
   description: "A To Do List on Ethereum Blockchain",
+  icons:{
+    icon:['/favicon.ico?v=4'],
+    apple:['/apple-touch-icon.png?v=4'],
+    shortcut:['/apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({ children }) {
