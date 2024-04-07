d9c720f99b6eee834f701a38bd02ad3ca09ff41f
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-04-07 11:32:43 +0200

    Update ToDoList.jsx

--- client/components/ToDoList.jsx
+++ client/components/ToDoList.jsx
@@ -4,20 +4,20 @@ import { FaPlusCircle } from "react-icons/fa";
 const ToDoList = () => {
 
   return (
-    <div className='flex items-center justify-center w-full p-10'>
+    <div className='flex items-center justify-center'style={{ height: '50vh' }}>
       <div className='w-[70%] bg-[#22223B] py-4 px-9 rounded-[20px]'>
-        {/* Titre */}
+
         <h2 className='text-4xl font-bold text-[#2F7DC8] pb-8'>To Do Chain</h2>
 
-        <div className='py-3 text-[#36A65F]'>Add a new task...</div>
-
         <form className='flex items-center justify-center'>
+        <input
+            className='rounded-[10px] w-full p-4 border-none outline-none text-[#36A65F]  bg-[#1d1d2b] '
+            placeholder='Add a task for today...'
+        />
           <FaPlusCircle
             className='text-[#EFB227] text-[50px] cursor-pointer ml-[20px] mb-[10px]'
           />
         </form>
-
-
       </div>
     </div>
   );
