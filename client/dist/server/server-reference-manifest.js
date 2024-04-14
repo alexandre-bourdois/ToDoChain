59536cc2d27b83f36629fa95087cc4844fbc7889
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-04-14 17:50:03 +0200

    Deploy

--- client/dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"7ORG1Ks10m6ckehNSUiXwh+vgfmoVOu3BZCMnD0PZM4=\"}"
--- client/next.config.mjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    distDir: 'dist',
-  };
-export default nextConfig;
